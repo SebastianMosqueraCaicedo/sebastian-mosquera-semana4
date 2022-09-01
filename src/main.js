ddf96eebd3a514a044de8e56942a9555465f2a8b
tabo <1005893565@u.icesi.edu.com>
2022-09-01 12:24:40 -0500

    add barebone-working structure

---
+++ src/main.js
@@ -0,0 +1,44 @@
+let allList = document.getElementById("all-list");
+let taskList = document.getElementById("task-list");
+let doneList = document.getElementById("done-list");
+
+let allArray = [];
+let taskArray = [];
+let doneArray = [];
+
+function handleChange(inputElement) {
+	// creates element on first list
+	let newItem = document.createElement("li");
+	newItem.addEventListener("click", (event) => handleClickListener(event));
+	const inputText = document.createTextNode(inputElement.value);
+	newItem.appendChild(inputText);
+	allList.appendChild(newItem);
+	
+	newItem.id = allArray.length;
+	allArray.push(newItem.id);
+
+	// adds the same element to the second list
+	taskClone = newItem.cloneNode(true);
+	taskClone.id = "T" + (allArray.length - 1);
+	taskList.appendChild(taskClone);
+	taskArray.push(taskClone.id);
+
+	// resets the input bar
+	document.getElementById("bar").value = null;
+	console.log(taskList);
+}
+
+function handleClickListener(event){
+	// adds the main element to the final list
+	let newDone = event.srcElement.cloneNode(true);
+	doneList.appendChild(newDone);
+	newDone.id = "D" + doneArray.length;
+	doneArray.push(newDone.id);
+	// removes element from second list
+	toRemove = document.getElementById("T" + event.srcElement.id)
+	toRemove.remove();
+	const found = taskArray.findIndex(element => element = "T" + event.srcElement.id);
+	taskArray.splice(found, 1);
+
+	event.srcElement.removeEventListener("click", (event) => handleClickListener(event));
+}
