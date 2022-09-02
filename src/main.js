4f61eacf0b381d00418875742f5fdb6f5aedd938
tabo <1005893565@u.icesi.edu.com>
2022-09-01 22:22:48 -0500

    fix "done-ing" the same task several times"

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ function handleChange(inputElement) {
 	allList.appendChild(newItem);
 	
 	newItem.id = allArray.length;
+	newItem.className = 'noClick'
 	allArray.push(newItem.id);
 
 	// adds the same element to the second list
@@ -29,16 +30,21 @@ function handleChange(inputElement) {
 }
 
 function handleClickListener(event){
-	// adds the main element to the final list
-	let newDone = event.srcElement.cloneNode(true);
-	doneList.appendChild(newDone);
-	newDone.id = "D" + doneArray.length;
-	doneArray.push(newDone.id);
-	// removes element from second list
-	toRemove = document.getElementById("T" + event.srcElement.id)
-	toRemove.remove();
-	const found = taskArray.findIndex(element => element = "T" + event.srcElement.id);
-	taskArray.splice(found, 1);
+	if (event.srcElement.className === 'noClick') {
+		// adds the main element to the final list
+		let newDone = event.srcElement.cloneNode(true);
+		doneList.appendChild(newDone);
+		newDone.id = "D" + doneArray.length;
+		doneArray.push(newDone.id);
+		// removes element from second list
+		toRemove = document.getElementById("T" + event.srcElement.id)
+		toRemove.remove();
+		const found = taskArray.findIndex(element => element = "T" + event.srcElement.id);
+		taskArray.splice(found, 1);
 
-	event.srcElement.removeEventListener("click", (event) => handleClickListener(event));
+		event.srcElement.removeEventListener("click", (event) => handleClickListener(event));
+		event.srcElement.className = 'click'
+
+	}
+	// if already clicked, do nothing
 }
